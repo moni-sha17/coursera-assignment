db8a265f87d205bdcbe80e00aa0436e582e761f8
Monisha S <monisha1852005@gmail.com>
2026-02-03 20:52:18 +0530

    plant.js

---
+++ plant.js
@@ -0,0 +1,8 @@
+export const plants = [
+  { id: 1, name: "Aloe Vera", price: 10, category: "Medicinal", img: "https://via.placeholder.com/100" },
+  { id: 2, name: "Snake Plant", price: 12, category: "Indoor", img: "https://via.placeholder.com/100" },
+  { id: 3, name: "Peace Lily", price: 15, category: "Indoor", img: "https://via.placeholder.com/100" },
+  { id: 4, name: "Tulsi", price: 8, category: "Medicinal", img: "https://via.placeholder.com/100" },
+  { id: 5, name: "Rose", price: 20, category: "Outdoor", img: "https://via.placeholder.com/100" },
+  { id: 6, name: "Money Plant", price: 11, category: "Indoor", img: "https://via.placeholder.com/100" },
+];
